80b4d8f63005ac73dfa3ff8a7c86a822d6699203
zrutao <1585240709@qq.com>
2020-07-06 14:26:06 +0800

    个人博客--后台创建

---
+++ src/test/java/com/item/blogserver/BlogserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.item.blogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
